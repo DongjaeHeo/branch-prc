95b0689372f2fdaab597c365454707de3fd34df8
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-22 11:37:21 +0900

    feat: Add buzz to fizz-buzz.py
    The buzz function will print "Buzz" if the number is divisible by 5 else print the number.

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,6 +1,6 @@
-def fizz(n):
-    for i in range(1, n +1):
-        if i % 3 == 0:
-            print('Fizz')
+def buzz(n):
+    for i in range(1, n + 1):
+        if i % 5 == 0:
+            print('Buzz')
         else:
             print(i)
